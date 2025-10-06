002154f410d3b28d3b1123a6002f2f6387df3f04
HEI240108 <HEI240108@spengergasse.at>
2025-10-06 12:52:44 +0200

    5 testfälle 2

--- main.ts
+++ main.ts
@@ -2,6 +2,20 @@ export function add(a: number, b: number): number {
   return a + b;
 }
 
+export function mult(a: number, b: number): number {
+  return a * b;
+}
+
+export function div(a: number, b: number): number {
+  return a / b;
+}
+export function sub(a: number, b: number): number {
+  return a - b;
+}
+export function sqr(a: number): number {
+  return Math.sqrt(9);
+}
+
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
   console.log("Add 2 + 3 =", add(2, 3));
--- main_test.ts
+++ main_test.ts
@@ -1,6 +1,23 @@
 import { assertEquals } from "@std/assert";
-import { add } from "./main.ts";
+import { add, mult, div, sub, sqr } from "./main.ts";
 
 Deno.test(function addTest() {
   assertEquals(add(2, 3), 5);
 });
+
+Deno.test(function multTest() {
+  assertEquals(mult(2, 3), 6);
+});
+
+Deno.test(function divTest() {
+  assertEquals(div(6, 3), 2);
+});
+
+Deno.test(function subTest() {
+  assertEquals(sub(5, 3), 2);
+});
+
+Deno.test(function sqrTest() {
+  assertEquals(sqr(9), 3);
+});
+
